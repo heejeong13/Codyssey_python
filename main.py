d1e30f71771a9f74c85d2f1065b2ae3589ada5d7
heejeong <kosigi19@gmail.com>
2026-04-18 21:19:24 +0900

    feat: json파일 로드 함수화 구현

--- main.py
+++ main.py
@@ -1,43 +1,41 @@
-import json
-from pathlib import Path
+
+from game import Game
+
+FILE_PATH = 'state.json'
 
 def main():
     try:
+        game = Game(FILE_PATH)
+
         while True:
-            print("=============")
+            print("\n" + "=" * 40)
             print("My Quiz")
-            print("=============")
+            print("=" * 40)
             print("1. 퀴즈 풀기")
             print("2. 퀴즈 추가")
             print("3. 퀴즈 목록")
             print("4. 점수 확인")
             print("5. 종료")
-            print("=============")
+            print("=" * 40)
             selectedNumber = input("번호를 선택하세요:").strip()
 
             if selectedNumber == '1':
-                BASE_DIR = Path(__file__).resolve().parent
-                STATE_FILE_PATH = BASE_DIR/"data"/'state.json'
-                print(STATE_FILE_PATH)
-
-                with open(STATE_FILE_PATH, 'r', encoding='utf-8') as f:
-                    data = json.load(f)
-                    high_score = data.get('best_score',0)
-                    quizzes = data.get('quizzes',[])
-                    for quiz in quizzes:
-                        print(quiz)
-                        print(quiz["question"])
+                """퀴즈 풀기"""
+                game.play()
             elif selectedNumber == '2':
+                """퀴즈 추가"""
                 print('2')
             elif selectedNumber == '3':
+                """퀴즈 목록"""
                 print('3')
             elif selectedNumber == '4':
+                """퀴즈 확인"""
                 print('4')
             elif selectedNumber == '5':
-                print('5')
+                """퀴즈 종료"""
                 break
             else:
-                print("!!!!! 1~5사이의 숫자를 입력하세요. !!!!!\n")
+                print("⚠️ 1~5사이의 숫자를 입력하세요.\n")
     except EOFError:
         print("프로그램을 종료합니다.")
     except KeyboardInterrupt:
