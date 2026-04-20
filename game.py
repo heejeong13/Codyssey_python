faf8d626796ded9849bcc8eba2b0c7106aa85a63
heejeong <kosigi19@gmail.com>
2026-04-20 19:46:57 +0900

    refactor: 메뉴 함수 메서드화

--- game.py
+++ game.py
@@ -11,6 +11,42 @@ class Game:
         self.quizzes = []
         self.best_score = 0
         self.load_quiz()
+    
+    def run_quiz(self):
+        try:
+            while True:
+                print("\n" + "=" * 40)
+                print("My Quiz")
+                print("=" * 40)
+                print("1. 퀴즈 풀기")
+                print("2. 퀴즈 추가")
+                print("3. 퀴즈 목록")
+                print("4. 점수 확인")
+                print("5. 종료")
+                print("=" * 40)
+                selectedNumber = input("번호를 선택하세요:").strip()
+
+                if selectedNumber == '1':
+                    """퀴즈 풀기"""
+                    game.play_quiz()
+                elif selectedNumber == '2':
+                    """퀴즈 추가"""
+                    game.add_quiz()
+                elif selectedNumber == '3':
+                    """퀴즈 목록"""
+                    game.view_quiz()
+                elif selectedNumber == '4':
+                    """점수 확인"""
+                    print('4')
+                elif selectedNumber == '5':
+                    """퀴즈 종료"""
+                    break
+                else:
+                    print("⚠️ 1~5사이의 숫자를 입력하세요.\n")
+        except EOFError:
+            print("프로그램을 종료합니다.")
+        except KeyboardInterrupt:
+            print("프로그램을 종료합니다.")
 
     def load_quiz(self):
         if not os.path.exists(self.file_path):
--- main.py
+++ main.py
@@ -4,42 +4,10 @@ from game import Game
 FILE_PATH = 'state.json'
 
 def main():
-    try:
-        game = Game(FILE_PATH)
+    game = Game(FILE_PATH)
+    game.run_quiz()
 
-        while True:
-            print("\n" + "=" * 40)
-            print("My Quiz")
-            print("=" * 40)
-            print("1. 퀴즈 풀기")
-            print("2. 퀴즈 추가")
-            print("3. 퀴즈 목록")
-            print("4. 점수 확인")
-            print("5. 종료")
-            print("=" * 40)
-            selectedNumber = input("번호를 선택하세요:").strip()
-
-            if selectedNumber == '1':
-                """퀴즈 풀기"""
-                game.play_quiz()
-            elif selectedNumber == '2':
-                """퀴즈 추가"""
-                game.add_quiz()
-            elif selectedNumber == '3':
-                """퀴즈 목록"""
-                game.view_quiz()
-            elif selectedNumber == '4':
-                """퀴즈 확인"""
-                print('4')
-            elif selectedNumber == '5':
-                """퀴즈 종료"""
-                break
-            else:
-                print("⚠️ 1~5사이의 숫자를 입력하세요.\n")
-    except EOFError:
-        print("프로그램을 종료합니다.")
-    except KeyboardInterrupt:
-        print("프로그램을 종료합니다.")
+       
 
 
 if __name__ == '__main__':
